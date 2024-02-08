a89a04b567ef154ea7a19e4b41e2fa1cb81719ea
sofiauldemiaclear <sofy.torresv@gmail.com>
2024-02-07 23:04:40 -0500

    Add:interacting with js to capture the value of the textarea

---
+++ app.js
@@ -0,0 +1,8 @@
+function obtenerValor() {
+    const textUser = document.querySelector("#textoUsuario").value;
+    console.log(textUser);
+}
+function obtenerValor() {
+    const textUser = document.querySelector("#textoUsuario").value;
+    console.log(textUser);
+}
