5c6e65f1d5c121ebd055135b2e18517b52e556b0
sofiauldemiaclear <sofy.torresv@gmail.com>
2024-02-12 16:23:18 -0500

    Add: hit the copy button and be able to decrypt

--- app.js
+++ app.js
@@ -1,29 +1,59 @@
 const inputText = document.querySelector("#user-text");
-const outputText = document.querySelector(".returned-message");
+const outputText = document.querySelector("#returned-message");
 
-function encryptText() {
-    const codedVowels = [
-        ["e", "enter"],
-        ["i", "imes"],
-        ["a", "ai"],
-        ["o", "ober"],
-        ["u", "ufat"],
-    ];
-}
+const codedVowels = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+];
 
 function handleEncryption() {
     const encryptedText = encrypt();
-    outputText.textContent = encryptedText;
-    inputText.value = "";
+    outputText.value = encryptedText;
+    clearInput();
 }
 
 function handleDecryption() {
-    const decryptedText = inputText.value.toLowerCase();
-    console.log(decryptedText);
+    const decryptedText = decrypt();
+    inputText.value = decryptedText;
+    clearInput();
 }
 
 function encrypt() {
-    const ingreso = inputText.value;
-    console.log(ingreso);
-    return ingreso;
+    let encryptMessage = inputText.value.toLowerCase();
+
+    for (let i = 0; i < codedVowels.length; i++) {
+        encryptMessage = encryptMessage.replaceAll(
+            codedVowels[i][0],
+            codedVowels[i][1]
+        );
+    }
+
+    console.log(encryptMessage);
+    return encryptMessage;
+}
+
+function decrypt() {
+    let decryptMessage = outputText.value.toLowerCase();
+
+    for (let i = 0; i < codedVowels.length; i++) {
+        decryptMessage = decryptMessage.replaceAll(
+            codedVowels[i][1],
+            codedVowels[i][0]
+        );
+    }
+
+    console.log(decryptMessage);
+    return decryptMessage;
+}
+
+function clearInput() {
+    inputText.value = "";
+}
+
+function handleCopy() {
+    const textCopiado = outputText.value;
+    navigator.clipboard.writeText(textCopiado);
 }
